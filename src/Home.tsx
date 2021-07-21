ea489313272738b42fdad83003aa5d1439d5fa51
Alyxia Sother <lexisoth2005@gmail.com>
2021-07-21 17:42:26 +0200

    [Home] Added base page

---
+++ src/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Page from './shared/Page';
+
+export default function HomePage(): JSX.Element {
+    return (
+        <Page>
+            <h1>hi</h1>
+        </Page>
+    )
+}
---
+++ src/shared/Page.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Helmet from "react-helmet";
+import "../css/index.css";
+
+interface MetaProps {
+    title?: string;
+    description?: string;
+    keywords?: string[];
+    imageUrl?: string;
+}
+function Meta ({title, description, keywords}: MetaProps): JSX.Element {
+    const defaults = {
+        title: "Nova",
+        description: "Nova website"
+    };
+
+    const actual = {
+        title: title ? `${title} | ${defaults.title}` : defaults.title,
+        description: description || defaults.description,
+        keywords: keywords?.join(", ")
+    };
+    return (
+        <Helmet>
+            <html lang="en" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+            <title>{actual.title}</title>
+
+            <meta name="description" content={actual.description} />
+            {actual.keywords && <meta name="keywords" content={actual.keywords} />}
+
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content={actual.title} />
+            <meta property="og:description" content={actual.description} />
+
+            <meta name="twitter:title" content={actual.title} />
+            <meta name="twitter:site" content="@lexisother" />
+            <meta name="twitter:creator" content="@lexisother" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:description" content={actual.description} />
+        </Helmet>
+    );
+}
+
+interface PageProps extends MetaProps {
+    children: React.ReactNode;
+}
+const Page = ({children, ...props}: PageProps): JSX.Element => {
+    return (
+        <div>
+            <Meta {...props} />
+
+            <main>{children}</main>
+        </div>
+    );
+};
+
+export default Page;
