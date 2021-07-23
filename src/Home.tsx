66109eb171d632af9e9135f7312269287667e849
Alyxia Sother <lexisoth2005@gmail.com>
2021-07-23 17:23:30 +0200

    [Home] Added services cards

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Page from "./shared/Page";
-import {GoRadioTower, GoHome} from "react-icons/go";
+import {GoRadioTower} from "react-icons/go";
+import {SiGitea, SiDrone} from "react-icons/si";
 
 export default function HomePage(): JSX.Element {
     return (
@@ -9,24 +10,46 @@ export default function HomePage(): JSX.Element {
                 <h1>Nova</h1>
             </div>
             <div className="content">
-                <p>Yes.</p>
+                <p>An """organization""" of friends creating projects for fun. We also host a couple of things.</p>
                 <div className="grid">
                     <div className="gridItem">
                         <div className="gridItemHeader">
                             <GoRadioTower />
-                            <p>Yes</p>
+                            <p>Funkwhale</p>
                         </div>
                         <div className="gridItemText">
-                            <p>Uh-huh. Yes.</p>
+                            <p>
+                                A private instance of the <a href="https://funkwhale.audio/">Funkwhale</a> audio player.
+                            </p>
+                            <p>
+                                Registrations are <i>closed</i>, and we aren't planning on opening them.
+                            </p>
                         </div>
                     </div>
                     <div className="gridItem">
                         <div className="gridItemHeader">
-                            <GoHome />
-                            <p>Uh-huh.</p>
+                            <SiGitea />
+                            <p>NovaGit</p>
                         </div>
                         <div className="gridItemText">
-                            <p>Yes, yes. Yes.</p>
+                            <p>
+                                An instance of <a href="https://gitea.com">Gitea</a> for Nova-related projects.
+                            </p>
+                            <p>
+                                Registrations are <i>closed</i>, because this is Nova-only.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="gridItem">
+                        <div className="gridItemHeader">
+                            <SiDrone />
+                            <p>Woodpecker (Drone)</p>
+                        </div>
+                        <div className="gridItemText">
+                            <p>
+                                An instance of the <a href="https://drone.io">Drone</a> fork{" "}
+                                <a href="https://github.com/woodpecker-ci/woodpecker">Woodpecker</a> for use on NovaGit.
+                            </p>
                         </div>
                     </div>
                 </div>
